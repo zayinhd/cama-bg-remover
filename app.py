c69c49e4eb2121b93b82c358177303d495e08053
zayin <zayinm300@gmail.com>
2024-12-27 10:02:46 +0000

    Fixing bug: Serverless error

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import io
 
 app = Flask(__name__)
 
+encoded_modified = None
+
 
 @app.route('/')
 def index():
@@ -13,25 +15,35 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    global encoded_modified
+
     uploaded_file = request.files['image']
     if uploaded_file.filename != '':
         input_image = uploaded_file.read()
         output_image = remove(input_image)
 
-    global encoded_modified
-    encoded_original = base64.b64encode(input_image).decode('utf-8')
-    encoded_modified = base64.b64encode(output_image).decode('utf-8')
-    return render_template("index.html", original_image=f'data:image/png;base64,{encoded_original}', modified_image=f'data:image/png;base64,{encoded_modified}')
+        encoded_original = base64.b64encode(input_image).decode('utf-8')
+        encoded_modified = base64.b64encode(output_image).decode('utf-8')
+        return render_template("index.html", original_image=f'data:image/png;base64,{encoded_original}', modified_image=f'data:image/png;base64,{encoded_modified}')
+
+    return "No file selected", 400
 
 
 @app.route('/upload/download')
 def download():
-    decoded_image = base64.b64decode(encoded_modified)
+    global encoded_modified  # Access the globally stored modified image
+    if encoded_modified is None:
+        return "No modified image available for download", 400
 
-    image_io = io.BytesIO(decoded_image)
+    try:
+        decoded_image = base64.b64decode(encoded_modified)
 
-    return send_file(
-        image_io, mimetype='image/png', as_attachment=True, download_name='cama-bg-remover.png')
+        image_io = io.BytesIO(decoded_image)
+
+        return send_file(
+            image_io, mimetype='image/png', as_attachment=True, download_name='cama-bg-remover.png')
+    except Exception as e:
+        return f"Error during file download: {e}", 500
 
 
 @app.route('/contact')
